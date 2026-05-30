685b5170f6723c912c98032be5954cf2d4603624
divyansh2901 <divyanshkashyap2901@gmail.com>
2026-05-30 23:34:36 +0530

    made some changes

---
+++ src/controllers/authController.js
@@ -0,0 +1,55 @@
+import prisma from "../config/prisma.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async(req,res) => {
+    try {
+        const {name,email,password} = req.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: {email},
+        });
+
+        if(existingUser) {
+            return res.status(400).json({
+                message: "User already exists",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        //generate token
+        const token = jwt.sign(
+            {id: user.id},
+            process.env.JWT_SECRET,{
+                expiresIn: "7d",
+            }
+        );
+
+        res.status(201).json({
+            message: "User registered successfully",
+            token,
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+            },
+        });
+    } catch(error){
+        console.log(error);
+
+        res.status(500).json({
+            message: "Server Error",
+        });
+    }
+};
+
+
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.use('/',(req,res)=>{
     res.send("DevFlow API Running");
